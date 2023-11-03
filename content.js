30a28372800b5d242bffc7f03377ba172389093d
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-02 20:08:04 -0400

    adding cookie code

--- content.js
+++ content.js
@@ -1,31 +1,53 @@
-// Change the background color of every even-indexed div
-let divs = document.getElementsByTagName('div');
-for (var i = 0; i < divs.length; i += 2) {
-  divs[i].style.backgroundColor = '#D81B60';
+
+// let getting = browser.cookies.get(
+//   details                // object
+// )
+
+
+// !!(await Cookies.get('my-cookie'));
+
+function getCookies(name, callback) {
+  chrome.cookies.get({ "name": name }, function (cookie) {
+    if (callback) {
+      callback(cookie.value);
+    }
+  });
 }
 
+//usage:
+getCookies("http://www.example.com", "id", function (id) {
+  alert(id);
+});
+
+// // Change the background color of every even-indexed div
+// let divs = document.getElementsByTagName('div');
+// for (var i = 0; i < divs.length; i += 2) {
+//   divs[i].style.backgroundColor = '#D81B60';
+// }
 
 
-// Change the background color of all paragraphs
-let paragraphs = document.getElementsByTagName('button');
-for (var i = 0; i < paragraphs.length; i++) {
-  paragraphs[i].style.backgroundColor = '#1E88E5';
-}
 
-// Change the background color of all h1 elements
-let h1 = document.getElementsByTagName('a');
-for (var i = 0; i < h1.length; i++) {
-  h1[i].style.backgroundColor = '#004D40';
-}
+// // Change the background color of all paragraphs
+// let paragraphs = document.getElementsByTagName('button');
+// for (var i = 0; i < paragraphs.length; i++) {
+//   paragraphs[i].style.backgroundColor = '#1E88E5';
+// }
 
-// Change the background color of the body
-document.body.style.backgroundColor = '#D81B60';
 
-// Change the background color of all h1 elements
-let span = document.getElementsByTagName('span');
-for (var i = 0; i < h1.length; i++) {
-  span[i].style.backgroundColor = '#1E88E5';
-}
+// // Change the background color of all h1 elements
+// let h1 = document.getElementsByTagName('a');
+// for (var i = 0; i < h1.length; i++) {
+//   h1[i].style.backgroundColor = '#004D40';
+// }
 
-// Change the background color of the body
-document.body.style.backgroundColor = '#D81B60';
+// // Change the background color of the body
+// document.body.style.backgroundColor = '#D81B60';
+
+// // Change the background color of all h1 elements
+// let span = document.getElementsByTagName('span');
+// for (var i = 0; i < h1.length; i++) {
+//   span[i].style.backgroundColor = '#1E88E5';
+// }
+
+// // Change the background color of the body
+// document.body.style.backgroundColor = '#D81B60';
